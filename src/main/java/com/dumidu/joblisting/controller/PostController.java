7fced40117d18faf23310b4b510716c0dc03070c
dkavish <dumidu805@gmail.com>
2024-04-22 02:08:59 +0530

    create search endpoint and cross to frontend

--- src/main/java/com/dumidu/joblisting/controller/PostController.java
+++ src/main/java/com/dumidu/joblisting/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.dumidu.joblisting.controller;
 
 import com.dumidu.joblisting.Repo.PostRepository;
+import com.dumidu.joblisting.Repo.SearchRepository;
 import com.dumidu.joblisting.model.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -11,11 +12,15 @@ import java.io.IOException;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class PostController
 {
 
     @Autowired
     PostRepository repo;
+
+    @Autowired
+    SearchRepository srepo;
 
 
     @ApiIgnore
@@ -24,16 +29,28 @@ public class PostController
         response.sendRedirect("/swagger-ui.html");
     }
 
-    @GetMapping("/posts")
+    @GetMapping("/allPosts")
+    @CrossOrigin
     public List<Post> getAllPosts()
     {
         return repo.findAll();
     }
 
+
+    @GetMapping("/posts/{text}")
+    @CrossOrigin
+    public List<Post> search(@PathVariable  String text)
+    {
+        return srepo.findByText(text);
+    }
+
     @PostMapping("/post")
+    @CrossOrigin
     public Post addPost(@RequestBody Post post)
     {
         return repo.save(post);
     }
+
+
 
 }
